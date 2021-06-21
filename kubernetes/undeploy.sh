1096ecac5d62924392acebfe41ca44471cd0aa1e
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-06-21 17:56:15 +0200

    fix undeploy.sh

--- kubernetes/undeploy.sh
+++ kubernetes/undeploy.sh
@@ -7,13 +7,10 @@ pushd . > /dev/null
 cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
-
 NAMESPACE=$(source $SCRIPT_PATH/config; echo $name)
-
+CHECK=$(kubectl get namespaces | grep $NAMESPACE | awk -F' ' '{ print $1 }')
 if  ([ "${CHECK}" == "${NAMESPACE}" ]) then
   $SCRIPT_PATH/templater.sh $SCRIPT_PATH/deployment.yaml.template -s -f $SCRIPT_PATH/config > $SCRIPT_PATH/deployment.yaml
-
   kubectl delete -f $SCRIPT_PATH/deployment.yaml
-
   kubectl delete namespace $NAMESPACE && rm -rf $SCRIPT_PATH/deployment.yaml
 fi
