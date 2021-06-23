1bd1c73532ce262635b8a9da9dad2c39cd87b89d
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-06-23 18:13:02 +0200

    download work

--- src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/api/impl/EServiceApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/api/impl/EServiceApiMarshallerImpl.scala
@@ -1,7 +1,8 @@
 package it.pagopa.pdnd.interop.uservice.catalogmanagement.api.impl
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.marshalling.ToEntityMarshaller
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
+import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.unmarshalling.{FromStringUnmarshaller, Unmarshaller}
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.api.EServiceApiMarshaller
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.{EService, Problem}
@@ -12,7 +13,9 @@ import scala.concurrent.Future
 
 class EServiceApiMarshallerImpl extends EServiceApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
 
-  override implicit def toEntityMarshallerFile: ToEntityMarshaller[File] = ???
+  //TODO this implicit is not really used, due usage of HttpEntity.fromFile
+  override implicit def toEntityMarshallerFile: ToEntityMarshaller[File] =
+    Marshaller.withFixedContentType(ContentTypes.`application/octet-stream`)(s => s.toString)
 
   override implicit def fromStringUnmarshallerStringList: FromStringUnmarshaller[Seq[String]] =
     Unmarshaller(_ => s => Future.successful(s.split(",").toSeq))
