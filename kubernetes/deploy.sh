51a59b2b1ec5490296f631195ede6bbf84574259
P3trur0 <ubaldopescatore@gmail.com>
2021-07-21 18:07:17 +0200

    fixed jenkins credentials

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -18,7 +18,7 @@ kubectl create secret generic cassandra --from-literal=CASSANDRA_HOST=$CASSANDRA
 
 kubectl create secret generic application.conf --from-file=application.conf=$SCRIPT_PATH/../src/main/resources/application.conf -n $NAMESPACE
 
-kubectl create secret generic aws --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -n $NAMESPACE
+kubectl create secret generic aws --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_USR --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_PSW -n $NAMESPACE
 
 $SCRIPT_PATH/templater.sh $SCRIPT_PATH/deployment.yaml.template -s -f $SCRIPT_PATH/config > $SCRIPT_PATH/deployment.yaml
 
