3321089bdc2e79a236b04564503b378c0478f217
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-07-26 19:19:38 +0200

    upgrade libs

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  lazy val akkaVersion                 = "2.6.14"
+  lazy val akkaVersion                 = "2.6.15"
   lazy val akkaHttpVersion             = "10.2.4"
   lazy val awsSdkVersion               = "2.4.17"
   lazy val projectionVersion           = "1.2.1"
@@ -7,7 +7,7 @@ object Versions {
   lazy val s3Persistence               = "1.0.27"
   lazy val httpJson4sVersion           = "1.37.0"
   lazy val akkaManagementVersion       = "1.0.10"
-  lazy val json4sVersion               = "4.0.1"
+  lazy val json4sVersion               = "4.0.3"
   lazy val catsVersion                 = "2.6.1"
   lazy val kamonVersion                = "2.1.17"
   lazy val openapi4jVersion            = "1.0.7"
