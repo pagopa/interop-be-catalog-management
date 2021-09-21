8efa6dd9f4fef2b0cbb85b382f31d3c7cad75abc
P3trur0 <ubaldopescatore@gmail.com>
2021-09-21 11:00:23 +0200

    missing journaled events added

--- src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/model/persistence/serializer/v1/package.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/model/persistence/serializer/v1/package.scala
@@ -1,16 +1,24 @@
 package it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer
 
 import cats.implicits._
+import it.pagopa.pdnd.interop.uservice.catalogmanagement.common._
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model._
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence._
-import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer.v1.catalog_item.CatalogItemV1
+import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer.v1.catalog_item.{
+  CatalogDocumentV1,
+  CatalogItemV1
+}
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer.v1.events.{
   CatalogItemV1AddedV1,
-  CatalogItemV1DeletedV1
+  CatalogItemV1DeletedV1,
+  CatalogItemV1UpdatedV1,
+  DocumentUpdatedV1
 }
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer.v1.state.{CatalogItemsV1, StateV1}
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.model.persistence.serializer.v1.utils._
 
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 package object v1 {
 
@@ -139,4 +147,82 @@ package object v1 {
         )
     }
 
+  implicit def catalogItemV1UpdatedV1PersistEventDeserializer
+    : PersistEventDeserializer[CatalogItemV1UpdatedV1, CatalogItemUpdated] =
+    event => {
+      for {
+        attributes  <- convertAttributesFromV1(event.catalogItem.attributes)
+        descriptors <- convertDescriptorsFromV1(event.catalogItem.descriptors)
+      } yield CatalogItemUpdated(catalogItem =
+        CatalogItem(
+          id = UUID.fromString(event.catalogItem.id),
+          producerId = UUID.fromString(event.catalogItem.producerId),
+          name = event.catalogItem.name,
+          description = event.catalogItem.description,
+          technology = event.catalogItem.technology,
+          attributes = attributes,
+          descriptors = descriptors
+        )
+      )
+
+    }
+
+  implicit def catalogItemV1UpdatedV1PersistEventSerializer
+    : PersistEventSerializer[CatalogItemUpdated, CatalogItemV1UpdatedV1] =
+    event => {
+      for {
+        descriptors <- convertDescriptorsToV1(event.catalogItem.descriptors)
+      } yield CatalogItemV1UpdatedV1
+        .of(
+          CatalogItemV1(
+            id = event.catalogItem.id.toString,
+            producerId = event.catalogItem.producerId.toString,
+            name = event.catalogItem.name,
+            description = event.catalogItem.description,
+            technology = event.catalogItem.technology,
+            attributes = convertAttributesToV1(event.catalogItem.attributes),
+            descriptors = descriptors
+          )
+        )
+    }
+
+  implicit def documentUpdatedV1PersistEventDeserializer: PersistEventDeserializer[DocumentUpdatedV1, DocumentUpdated] =
+    event => {
+      for {
+        documentId <- event.updatedDocument.id.parseUUID
+      } yield DocumentUpdated(
+        eServiceId = event.eServiceId,
+        descriptorId = event.descriptorId,
+        documentId = event.documentId,
+        updatedDocument = CatalogDocument(
+          id = documentId,
+          name = event.updatedDocument.name,
+          contentType = event.updatedDocument.contentType,
+          description = event.updatedDocument.description,
+          path = event.updatedDocument.path,
+          checksum = event.updatedDocument.checksum,
+          uploadDate = OffsetDateTime.parse(event.updatedDocument.uploadDate, DateTimeFormatter.ISO_DATE_TIME)
+        )
+      )
+    }
+
+  implicit def documentUpdatedV1PersistEventSerializer: PersistEventSerializer[DocumentUpdated, DocumentUpdatedV1] =
+    event => {
+      Right[Throwable, DocumentUpdatedV1](
+        DocumentUpdatedV1(
+          eServiceId = event.eServiceId,
+          descriptorId = event.descriptorId,
+          documentId = event.documentId,
+          updatedDocument = CatalogDocumentV1(
+            id = event.updatedDocument.id.toString,
+            name = event.updatedDocument.name,
+            contentType = event.updatedDocument.contentType,
+            description = event.updatedDocument.description,
+            path = event.updatedDocument.path,
+            checksum = event.updatedDocument.checksum,
+            uploadDate = event.updatedDocument.uploadDate.format(DateTimeFormatter.ISO_DATE_TIME)
+          )
+        )
+      )
+    }
 }
