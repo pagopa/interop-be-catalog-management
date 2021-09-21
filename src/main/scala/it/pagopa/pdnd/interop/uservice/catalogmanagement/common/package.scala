ff47ec8effa714660dd9f157e56f9cf9b1e119fe
Ubaldo Pescatore <P3trur0@users.noreply.github.com>
2021-09-21 11:31:37 +0200

    missing journaled events added (#12)

--- src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/common/package.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/catalogmanagement/common/package.scala
@@ -1,6 +1,8 @@
 package it.pagopa.pdnd.interop.uservice.catalogmanagement
 
+import java.util.UUID
 import scala.concurrent.Future
+import scala.util.Try
 
 package object common {
   implicit class OptionOps[A](val option: Option[A]) extends AnyVal {
@@ -9,5 +11,9 @@ package object common {
 
   implicit class EitherOps[E <: Throwable, A](val either: Either[E, A]) extends AnyVal {
     def toFuture: Future[A] = either.fold[Future[A]](Future.failed[A], Future.successful)
+  }
+
+  implicit class StringOps(val str: String) extends AnyVal {
+    def parseUUID: Either[Throwable, UUID] = Try { UUID.fromString(str) }.toEither
   }
 }
