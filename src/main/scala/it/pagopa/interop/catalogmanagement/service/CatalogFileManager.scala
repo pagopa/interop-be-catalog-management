6336bbbfd5f3292d06fb81a0533465da853a1353
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-03-09 09:48:18 +0100

    add log to verifyTechnology function (#61)

--- src/main/scala/it/pagopa/interop/catalogmanagement/service/CatalogFileManager.scala
+++ src/main/scala/it/pagopa/interop/catalogmanagement/service/CatalogFileManager.scala
@@ -1,14 +1,17 @@
 package it.pagopa.interop.catalogmanagement.service
 
 import akka.http.scaladsl.server.directives.FileInfo
+import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.catalogmanagement.error.CatalogManagementErrors.{
   DocumentAlreadyUploaded,
   InvalidInterfaceFileDetected
 }
 import it.pagopa.interop.catalogmanagement.model.{CatalogDocument, CatalogItem, Rest, Soap}
 import it.pagopa.interop.commons.files.service.{FileManager, StorageFilePath}
+import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 import it.pagopa.interop.commons.utils.Digester
 import org.apache.tika.Tika
+import org.slf4j.LoggerFactory
 
 import java.io.File
 import java.nio.file.Files
@@ -33,10 +36,14 @@ trait CatalogFileManager {
 
 object CatalogFileManager {
 
+  private val logger: LoggerTakingImplicit[ContextFieldsToLog] =
+    Logger.takingImplicit[ContextFieldsToLog](LoggerFactory.getLogger(this.getClass))
+
   private final val tika: Tika = new Tika()
 
   def verify(fileParts: (FileInfo, File), catalogItem: CatalogItem, descriptorId: String, isInterface: Boolean)(implicit
-    ec: ExecutionContext
+    ec: ExecutionContext,
+    contexts: Seq[(String, String)]
   ): Future[CatalogItem] = for {
     checksumVerified <- verifyChecksum(fileParts, catalogItem, descriptorId)
     technologyVerified <-
@@ -61,11 +68,15 @@ object CatalogFileManager {
     else Future.successful(catalogItem)
   }
 
-  private def verifyTechnology(fileParts: (FileInfo, File), catalogItem: CatalogItem): Future[CatalogItem] = {
+  private def verifyTechnology(fileParts: (FileInfo, File), catalogItem: CatalogItem)(implicit
+    contexts: Seq[(String, String)]
+  ): Future[CatalogItem] = {
     val restContentTypes: Set[String] = Set("text/x-yaml", "application/x-yaml")
     val soapContentTypes: Set[String] = Set("application/soap+xml", "application/wsdl+xml")
 
     val detectedContentTypes: String = tika.detect(Files.readAllBytes(fileParts._2.toPath), fileParts._1.fileName)
+
+    logger.debug(s"Detected $detectedContentTypes interface content type for eservice: ${catalogItem.id}")
 
     val isValidTechnology = catalogItem.technology match {
       case Rest => restContentTypes.contains(detectedContentTypes)
