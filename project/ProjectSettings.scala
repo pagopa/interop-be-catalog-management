1346451889381c50e4eaf09441dd06ee54cbf12b
Antonio Gelameris <toniogela89@gmail.com>
2022-03-10 17:02:44 +0100

    [PIN-1124] Fixing compute version

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -7,7 +7,7 @@ import scala.sys.process._
 import scala.util.Try
 
 /** Allows customizations of build.sbt syntax.
- */
+  */
 object ProjectSettings {
 
   //TODO since Git 2.22 we could use the following command instead: git branch --show-current
@@ -18,8 +18,8 @@ object ProjectSettings {
   private val commitSha: Option[String] = Try(Process(s"git rev-parse --short HEAD").lineStream_!.head).toOption
 
   private val interfaceVersion: String = ComputeVersion.version.split("\\.") match {
-    case Array(major, minor, _*) =>
-      s"$major.$minor"
+    case Array(major, minor, _*) => s"$major.$minor"
+    case _                       => "0.0"
   }
 
   //lifts some useful data in BuildInfo instance
@@ -31,8 +31,8 @@ object ProjectSettings {
   )
 
   /** Extention methods for sbt Project instances.
-   * @param project
-   */
+    * @param project
+    */
   implicit class ProjectFrom(project: Project) {
     def setupBuildInfo: Project = {
       project
