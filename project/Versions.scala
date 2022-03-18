ead7aee7ba560781d3d1d26b13ccdc39f7a5d719
Scala Steward <scala-steward@pagopa.it>
2022-03-18 14:12:00 +0000

    Update akka-persistence-s3-journal, ... to 1.2.23 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object Versions {
   lazy val mustacheVersion             = "0.9.0"
   lazy val openapi4jVersion            = "1.0.7"
   lazy val projectionVersion           = "1.2.1"
-  lazy val s3Persistence               = "1.0.27"
+  lazy val s3Persistence               = "1.2.23"
   lazy val scalaMockVersion            = "5.1.0"
   lazy val scalaPactVersion            = "3.3.2-RC2"
   lazy val scalapbVersion              = "0.11.1"
