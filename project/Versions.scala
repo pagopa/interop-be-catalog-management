51678855519de9476ecbab57f3809a59c8100e9c
Antonio Gelameris <toniogela89@gmail.com>
2022-03-22 10:48:55 +0100

    Rob Norris is now happy

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,6 @@ object Versions {
   lazy val projectionVersion           = "1.2.1"
   lazy val s3Persistence               = "1.0.27"
   lazy val scalaMockVersion            = "5.2.0"
-  lazy val scalaPactVersion            = "4.4.0"
   lazy val scalapbVersion              = "0.11.1"
   lazy val scalatestVersion            = "3.2.11"
   lazy val slickProjectionVersion      = "1.2.2"
--- src/main/scala/it/pagopa/interop/catalogmanagement/model/persistence/serializer/StateSerializer.scala
+++ src/main/scala/it/pagopa/interop/catalogmanagement/model/persistence/serializer/StateSerializer.scala
@@ -16,15 +16,19 @@ class StateSerializer extends SerializerWithStringManifest {
 
   override def manifest(o: AnyRef): String = s"${o.getClass.getName}|$currentVersion"
 
-  final val StateManifest: String = classOf[State].getName
+  final val className: String = classOf[State].getName
 
   override def toBinary(o: AnyRef): Array[Byte] = o match {
-    case s: State => serialize(s, StateManifest, currentVersion)
+    case s: State => serialize(s, className, currentVersion)
+    case _ =>
+      throw new NotSerializableException(
+        s"Unable to serialize object of type [[${o.getClass.getName}]] for manifest [[$className]] and version [[$currentVersion]]"
+      )
   }
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef =
     manifest.split('|').toList match {
-      case StateManifest :: `version1` :: Nil =>
+      case `className` :: `version1` :: Nil =>
         deserialize(v1.state.StateV1, bytes, manifest, currentVersion)
       case _ =>
         throw new NotSerializableException(
