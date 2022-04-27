eed137b9bfa0dbf01d9abe80bf9c46914c90c7fc
galales <25105748+galales@users.noreply.github.com>
2022-04-27 15:45:59 +0200

    pin-1382: Remove lazy evaluation

--- src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
@@ -4,21 +4,21 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 object ApplicationConfiguration {
 
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("catalog-management.port")
+  val serverPort: Int = config.getInt("catalog-management.port")
 
-  lazy val storageContainer: String = config.getString("catalog-management.storage.container")
+  val storageContainer: String = config.getString("catalog-management.storage.container")
 
-  lazy val eserviceDocsPath: String = config.getString("catalog-management.storage.eservice-docs-path")
+  val eserviceDocsPath: String = config.getString("catalog-management.storage.eservice-docs-path")
 
-  lazy val jwtAudience: Set[String] =
+  val jwtAudience: Set[String] =
     config.getString("catalog-management.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
-  lazy val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
+  val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
 
   def projectionTag(index: Int)        = s"interop-be-catalog-management-persistence|$index"
-  lazy val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+  val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
 
   require(jwtAudience.nonEmpty, "Audience cannot be empty")
 
