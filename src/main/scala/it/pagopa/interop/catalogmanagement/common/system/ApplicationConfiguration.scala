fe922c0df7347d9c612cdad159fe3758ad37cbba
galales <25105748+galales@users.noreply.github.com>
2022-04-27 15:39:42 +0200

    pin-1382: Comma sperated strings for accepted audiences

--- src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
@@ -2,8 +2,6 @@ package it.pagopa.interop.catalogmanagement.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters.ListHasAsScala
-
 object ApplicationConfiguration {
 
   lazy val config: Config = ConfigFactory.load()
@@ -14,11 +12,14 @@ object ApplicationConfiguration {
 
   lazy val eserviceDocsPath: String = config.getString("catalog-management.storage.eservice-docs-path")
 
-  lazy val jwtAudience: Set[String] = config.getStringList("catalog-management.jwt.audience").asScala.toSet
+  lazy val jwtAudience: Set[String] =
+    config.getString("catalog-management.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
   lazy val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
 
   def projectionTag(index: Int)        = s"interop-be-catalog-management-persistence|$index"
   lazy val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+
+  require(jwtAudience.nonEmpty, "Audience cannot be empty")
 
 }
