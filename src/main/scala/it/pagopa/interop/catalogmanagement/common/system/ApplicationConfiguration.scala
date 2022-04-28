010a6e69483011f0b0c1ea17ee0a2d59442f37ad
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 09:39:15 +0200

    pin-1382: Comma separated strings for accepted audiences (#120)

--- src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/catalogmanagement/common/system/ApplicationConfiguration.scala
@@ -2,23 +2,24 @@ package it.pagopa.interop.catalogmanagement.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters.ListHasAsScala
-
 object ApplicationConfiguration {
 
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("catalog-management.port")
+  val serverPort: Int = config.getInt("catalog-management.port")
 
-  lazy val storageContainer: String = config.getString("catalog-management.storage.container")
+  val storageContainer: String = config.getString("catalog-management.storage.container")
 
-  lazy val eserviceDocsPath: String = config.getString("catalog-management.storage.eservice-docs-path")
+  val eserviceDocsPath: String = config.getString("catalog-management.storage.eservice-docs-path")
 
-  lazy val jwtAudience: Set[String] = config.getStringList("catalog-management.jwt.audience").asScala.toSet
+  val jwtAudience: Set[String] =
+    config.getString("catalog-management.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
-  lazy val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
+  val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
 
-  def projectionTag(index: Int)        = s"interop-be-catalog-management-persistence|$index"
-  lazy val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+  def projectionTag(index: Int)   = s"interop-be-catalog-management-persistence|$index"
+  val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+
+  require(jwtAudience.nonEmpty, "Audience cannot be empty")
 
 }
