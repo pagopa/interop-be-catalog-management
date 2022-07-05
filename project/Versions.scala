01cd2850401e90ca53fa075321c4cc4b4afb236f
Antonio Gelameris <toniogela89@gmail.com>
2022-07-05 17:58:46 +0200

    manual update

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,6 @@ object Versions {
   lazy val projectionVersion           = "1.2.1"
   lazy val s3Persistence               = "1.0.27"
   lazy val scalaMockVersion            = "5.2.0"
-  lazy val scalapbVersion              = "0.11.1"
   lazy val scalatestVersion            = "3.2.12"
   lazy val slickProjectionVersion      = "1.2.2"
   lazy val tikaVersion                 = "2.4.1"
