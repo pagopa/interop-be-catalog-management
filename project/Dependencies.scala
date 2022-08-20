b9449a8e624c0659f035aedfb5701a8181408b0b
Scala Steward <scala-steward@pagopa.it>
2022-08-20 06:47:55 +0000

    Update postgresql to 42.4.2 in 1.0.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
 
   private[this] object postgres {
     lazy val namespace = "org.postgresql"
-    lazy val jdbc      = namespace % "postgresql" % "42.4.0"
+    lazy val jdbc      = namespace % "postgresql" % "42.4.2"
   }
 
   lazy val Protobuf = "protobuf"
