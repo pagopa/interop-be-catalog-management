733580e418b635a53cf62586232fe4dbc4a4b44e
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-08-24 16:58:55 +0200

    Revert "1.0.x: Add testcontainers deps"
    This reverts commit 5e67422df55cabfac25891468aa9f339d6d6d8dd.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -149,8 +149,7 @@ object Dependencies {
       scalatest.core               % "test,it",
       "org.scalameta"             %% "munit-scalacheck"               % "0.7.29"                   % Test,
       "com.softwaremill.diffx"    %% "diffx-munit"                    % "0.7.0"                    % Test,
-      "com.dimafeng"              %% "testcontainers-scala-scalatest" % testcontainersScalaVersion % IntegrationTest,
-      "org.scoverage" % "scalac-scoverage-plugin" % "2.0.0"% "provided"
+      "com.dimafeng"              %% "testcontainers-scala-scalatest" % testcontainersScalaVersion % IntegrationTest
     )
     lazy val client: List[ModuleID]    =
       List(akka.stream, akka.http, akka.httpJson4s, akka.slf4j, json4s.jackson, json4s.ext, pagopa.commonsUtils).map(
