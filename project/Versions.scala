9974062efe467670aac2578941529fdea4ceb135
Scala Steward <scala-steward@pagopa.it>
2022-10-05 15:15:10 +0000

    Update tika-core to 2.5.0 in 1.0.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
   lazy val scalaMockVersion            = "5.2.0"
   lazy val scalatestVersion            = "3.2.12"
   lazy val slickProjectionVersion      = "1.2.2"
-  lazy val tikaVersion                 = "2.4.1"
+  lazy val tikaVersion                 = "2.5.0"
   lazy val testcontainersScalaVersion  = "0.40.9"
 }
 
